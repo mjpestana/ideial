baef87b0fb3faf2fa4f58785c23771c7a93d9e3b
Teresa Paulino <teresampaulino@hotmail.com>
2016-01-03 17:10:00 +0000

    Criar campanha Form - Design atualizado

--- ideial/View/CriarCampanha_frm.Designer.cs
+++ ideial/View/CriarCampanha_frm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CriarCampanha_frm));
             this.descricao_txt = new System.Windows.Forms.TextBox();
             this.nome_txt = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,88 +42,108 @@
             // 
             // descricao_txt
             // 
-            this.descricao_txt.Location = new System.Drawing.Point(152, 55);
+            this.descricao_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descricao_txt.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.descricao_txt.Location = new System.Drawing.Point(155, 70);
             this.descricao_txt.Multiline = true;
             this.descricao_txt.Name = "descricao_txt";
-            this.descricao_txt.Size = new System.Drawing.Size(234, 101);
+            this.descricao_txt.Size = new System.Drawing.Size(266, 101);
             this.descricao_txt.TabIndex = 16;
             // 
             // nome_txt
             // 
-            this.nome_txt.Location = new System.Drawing.Point(152, 23);
+            this.nome_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nome_txt.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.nome_txt.Location = new System.Drawing.Point(155, 33);
             this.nome_txt.Name = "nome_txt";
-            this.nome_txt.Size = new System.Drawing.Size(234, 20);
+            this.nome_txt.Size = new System.Drawing.Size(266, 26);
             this.nome_txt.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 63);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(46, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(80, 20);
             this.label2.TabIndex = 14;
             this.label2.Text = "Descrição";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 31);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(75, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 13;
             this.label1.Text = "Nome";
             // 
             // data_lbl
             // 
             this.data_lbl.AutoSize = true;
-            this.data_lbl.Location = new System.Drawing.Point(149, 203);
+            this.data_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data_lbl.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.data_lbl.Location = new System.Drawing.Point(155, 213);
             this.data_lbl.Name = "data_lbl";
-            this.data_lbl.Size = new System.Drawing.Size(30, 13);
+            this.data_lbl.Size = new System.Drawing.Size(44, 20);
             this.data_lbl.TabIndex = 20;
             this.data_lbl.Text = "Data";
             // 
             // nomeUtilizador_lbl
             // 
             this.nomeUtilizador_lbl.AutoSize = true;
-            this.nomeUtilizador_lbl.Location = new System.Drawing.Point(149, 176);
+            this.nomeUtilizador_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nomeUtilizador_lbl.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.nomeUtilizador_lbl.Location = new System.Drawing.Point(155, 186);
             this.nomeUtilizador_lbl.Name = "nomeUtilizador_lbl";
-            this.nomeUtilizador_lbl.Size = new System.Drawing.Size(81, 13);
+            this.nomeUtilizador_lbl.Size = new System.Drawing.Size(121, 20);
             this.nomeUtilizador_lbl.TabIndex = 19;
             this.nomeUtilizador_lbl.Text = "Nome Utilizador";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(39, 203);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label7.Location = new System.Drawing.Point(82, 213);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.Size = new System.Drawing.Size(44, 20);
             this.label7.TabIndex = 18;
             this.label7.Text = "Data";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(39, 176);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(51, 186);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.Size = new System.Drawing.Size(75, 20);
             this.label6.TabIndex = 17;
             this.label6.Text = "Utilizador";
             // 
             // criarCampanha_btn
             // 
-            this.criarCampanha_btn.Location = new System.Drawing.Point(204, 269);
+            this.criarCampanha_btn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.criarCampanha_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.criarCampanha_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.criarCampanha_btn.Location = new System.Drawing.Point(170, 271);
             this.criarCampanha_btn.Name = "criarCampanha_btn";
-            this.criarCampanha_btn.Size = new System.Drawing.Size(102, 23);
+            this.criarCampanha_btn.Size = new System.Drawing.Size(160, 38);
             this.criarCampanha_btn.TabIndex = 21;
             this.criarCampanha_btn.Text = "Criar Campanha";
-            this.criarCampanha_btn.UseVisualStyleBackColor = true;
+            this.criarCampanha_btn.UseVisualStyleBackColor = false;
             this.criarCampanha_btn.Click += new System.EventHandler(this.criarCampanha_btn_Click);
             // 
             // CriarCampanha_frm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 358);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(484, 361);
             this.Controls.Add(this.criarCampanha_btn);
             this.Controls.Add(this.data_lbl);
             this.Controls.Add(this.nomeUtilizador_lbl);
@@ -132,6 +153,8 @@
             this.Controls.Add(this.nome_txt);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CriarCampanha_frm";
             this.Text = "Criar Campanha";
             this.Load += new System.EventHandler(this.CriarCampanha_frm_Load);
